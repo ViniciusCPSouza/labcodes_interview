e2a147d124b1b0e4948e62800f8530acdf0aacd7
ViniciusCPSouza <vcps@cin.ufpe.br>
2016-02-24 19:12:05 -0300

    Adding deploy-related data

--- todo_social/settings.py
+++ todo_social/settings.py
@@ -77,15 +77,16 @@ WSGI_APPLICATION = 'todo_social.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {}
 DATABASES['default'].update(db_from_env)
 
 
