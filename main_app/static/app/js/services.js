935eec2eb1ea46b6885a8f486f260ac2be3722c3
ViniciusCPSouza <vcps@cin.ufpe.br>
2016-02-24 19:34:58 -0300

    Fixing urls

--- main_app/static/app/js/services.js
+++ main_app/static/app/js/services.js
@@ -4,6 +4,11 @@ appServices.service("GetFromREST", function($http)
 {
     this.get = function(url)
     {
+        if (url.indexOf("https") <= -1 && url.indexOf("http") > -1)
+        {
+            url = url.replace("http", "https");
+        }
+
         return $http.get(url).then(function(response)
         {
             return response.data;
