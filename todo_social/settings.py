f96355cabb5088e915cd6a7c4481e1758122d5f6
ViniciusCPSouza <vcps@cin.ufpe.br>
2016-03-02 16:32:30 -0300

    Adding the local settings

--- todo_social/settings.py
+++ todo_social/settings.py
@@ -78,13 +78,6 @@ WSGI_APPLICATION = 'todo_social.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 import dj_database_url
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=500)
@@ -154,3 +147,13 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# ------------- Local Settings -------------
+
+try:
+
+    from local_settings import *
+
+except ImportError:
+
+    pass
